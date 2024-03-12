f2d3332d01afb7b30d90e6b6f98208dfa4d6bdb2
Gene7Him <155666233+Gene7Him@users.noreply.github.com>
2024-03-12 16:19:51 -0700

    pages updated

--- dataProcessing/header.php
+++ dataProcessing/header.php
@@ -1,6 +1,6 @@
 <?php
 // header.php
-
+setcookie("guestbook_greeting", "Hello, $name!", time() + 3600, "/", "gene7@greenriverdev.com");
 // Check if the greeting cookie is set
 if (isset($_COOKIE["guestbook_greeting"])) {
     $greeting = $_COOKIE["guestbook_greeting"];
